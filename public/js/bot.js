7cd41ced2cce3e88c2ceed9bbdd1d58b3ae8ee3b
jony <jony@setuptech.pt>
2023-08-01 23:24:50 +0600

    color change

--- public/js/bot.js
+++ public/js/bot.js
@@ -1,5 +1,5 @@
  var variableValue = document.querySelector('script[data-widget]').dataset.widget;
 
     window.justAddBotsSettings = { widget_id: variableValue };
-    !function(){var t=window,e=document,a=function(){a.init(arguments)},n=window.justAddBotsSettings.base_url||"https://chat.docs2ai.com";t.Cody=a;var i=function(){var t=e.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://docs2ai.com/js/widget.js";var a=e.getElementsByTagName("script")[0];a.parentNode.insertBefore(t,a)};"complete"===document.readyState?i():t.attachEvent?t.attachEvent("onload",i):t.addEventListener("load",i,!1)}();
+    !function(){var t=window,e=document,a=function(){a.init(arguments)},n=window.justAddBotsSettings.base_url||"https://chat.aibotbuild.com";t.Cody=a;var i=function(){var t=e.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://docs2ai.com/js/widget.js";var a=e.getElementsByTagName("script")[0];a.parentNode.insertBefore(t,a)};"complete"===document.readyState?i():t.attachEvent?t.attachEvent("onload",i):t.addEventListener("load",i,!1)}();
 
