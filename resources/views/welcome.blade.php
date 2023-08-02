6fad0acc676666f5795daebd308e86bce22bda3a
jony <jony@setuptech.pt>
2023-08-02 23:22:04 +0600

    color change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,16 @@
     <link rel="stylesheet" href="{{asset('frontend')}}/assets/css/animated.css">
     <link rel="stylesheet" href="{{asset('frontend')}}/assets/css/owl.css">
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TJTRXEDH9Z"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-TJTRXEDH9Z');
+    </script>
+
 </head>
 
 <body>
